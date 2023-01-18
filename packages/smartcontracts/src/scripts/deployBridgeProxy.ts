4ea72d84e02178006a8b15aee13dd65d886e383b
Abhishek <abhishekkochar2@gmail.com>
2023-01-18 13:29:48 +0800

    Final deployment scripts

--- packages/smartcontracts/src/scripts/deployBridgeProxy.ts
+++ packages/smartcontracts/src/scripts/deployBridgeProxy.ts
@@ -2,12 +2,12 @@ import { ethers } from 'hardhat';
 
 import { BridgeV1__factory } from '../generated';
 
-const ADMIN_ADDRESS = '';
-const OPERATIONAL_ADDRESS = '';
-const RELAYER_ADDRESS = '';
+const ADMIN_ADDRESS = '0x5aB853A40b3b9A16891e8bc8e58730AE3Ec102b2';
+const OPERATIONAL_ADDRESS = '0xb210F3bec24D034EF23fD1B3cDbE86a239527dF0';
+const RELAYER_ADDRESS = '0x5aB853A40b3b9A16891e8bc8e58730AE3Ec102b2';
 const TRANSACTION_FEE = 30;
-const BRIDGE_IMPLEMENTATION_ADDRESS = '';
-// npx hardhat run --network goerli ./scripts/deployBridgeImplementation.ts
+const BRIDGE_IMPLEMENTATION_ADDRESS = '0xE029B5156c2e597c72f7c8D279411e1fD9a30126';
+// npx hardhat run --network goerli ./scripts/deployBridgeProxy.ts
 async function main() {
   const BridgeProxy = await ethers.getContractFactory('BridgeProxy');
   const encodedData = BridgeV1__factory.createInterface().encodeFunctionData('initialize', [
@@ -32,3 +32,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// proxy contract address: https://goerli.etherscan.io/address/0x93fE70235854e7c97A5db5ddfC6eAAb078e99d3C
--- packages/smartcontracts/src/scripts/deployERC20.ts
+++ packages/smartcontracts/src/scripts/deployERC20.ts
@@ -1,20 +1,19 @@
 import { ethers } from 'hardhat';
 
 // npx hardhat run --network goerli ./scripts/deployERC20.ts
-const TOKEN_ADMIN_ADDRESS = '';
 async function main() {
-  const ERC20 = await ethers.getContractFactory('GoerliTestToken');
-  const mockTokenUSDT = await ERC20.deploy('Mock USDT', 'MUSDT', TOKEN_ADMIN_ADDRESS);
+  const ERC20 = await ethers.getContractFactory('TestToken');
+  const mockTokenUSDT = await ERC20.deploy('MockUSDT', 'MUSDT'); // use {nonce:} if tx stuck
   await mockTokenUSDT.deployed();
   console.log('Test token is deployed to ', mockTokenUSDT.address);
   console.log(
-    `To verify on Etherscan: npx hardhat verify --network goerli --contract contracts/GoerliTestToken.sol:GoerliTestToken ${mockTokenUSDT.address} Test T ${TOKEN_ADMIN_ADDRESS}`,
+    `To verify on Etherscan: npx hardhat verify --network goerli --contract contracts/TestToken.sol:TestToken ${mockTokenUSDT.address} MockUSDT MUSDT`,
   );
-  const mockTokenUSDC = await ERC20.deploy('Mock USDC', 'MUSDC', TOKEN_ADMIN_ADDRESS);
+  const mockTokenUSDC = await ERC20.deploy('MockUSDC', 'MUSDC');
   await mockTokenUSDC.deployed();
   console.log('Test token is deployed to ', mockTokenUSDC.address);
   console.log(
-    `To verify on Etherscan: npx hardhat verify --network goerli --contract contracts/GoerliTestToken.sol:GoerliTestToken ${mockTokenUSDC.address} Test T ${TOKEN_ADMIN_ADDRESS}`,
+    `To verify on Etherscan: npx hardhat verify --network goerli --contract contracts/TestToken.sol:TestToken ${mockTokenUSDC.address} MockUSDC MUSDC`,
   );
 }
 
@@ -22,3 +21,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// MUSDT Contract address 0xA218A0EA9a888e3f6E2dfFdf4066885f596F07bF - https://goerli.etherscan.io/address/0xA218A0EA9a888e3f6E2dfFdf4066885f596F07bF#code
+// MUSDC Contract address 0xB200af2b733B831Fbb3d98b13076BC33F605aD58 - https://goerli.etherscan.io/address/0xB200af2b733B831Fbb3d98b13076BC33F605aD58#code
