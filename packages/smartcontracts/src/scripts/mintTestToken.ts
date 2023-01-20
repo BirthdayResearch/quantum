a4d9d8f3fc398642f91b2af795720680127c9888
Abhishek <abhishekkochar2@gmail.com>
2023-01-20 13:37:25 +0800

    Minting and approve script

--- packages/smartcontracts/src/scripts/mintTestToken.ts
+++ packages/smartcontracts/src/scripts/mintTestToken.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 
 import { TestToken__factory } from '../generated';
@@ -7,41 +7,34 @@ require('dotenv').config({
   path: './.env',
 });
 
+// This script will mint and approve the Bridge address, given user has provided their PRIVATE_KEY in .env. See .env.example for the reference.
+// 100,000 tokens will be minted. `amount` can be changed. To run this script, run the below command in smartContract directory.
+// npx hardhat run --network goerli ./scripts/mintTestToken.ts
 async function main() {
   const usdcAddress = '0xB200af2b733B831Fbb3d98b13076BC33F605aD58';
   const usdtAddress = '0xA218A0EA9a888e3f6E2dfFdf4066885f596F07bF';
+  // Minting 100,000 tokens.
+  const amount = ethers.utils.parseEther('1');
+  // Minting M-USDT
+  await mintAndApproveTestTokens(usdtAddress, amount);
+  // Minting M-USDC
+  await mintAndApproveTestTokens(usdcAddress, amount);
+}
+
+async function mintAndApproveTestTokens(tokenAddress: string, amount: BigNumber) {
+  const bridgeAddress = '0x93fE70235854e7c97A5db5ddfC6eAAb078e99d3C';
   const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  // const signer = provider.ge//(wallet.address);
-  const mUsdcContract = new ethers.Contract(usdtAddress, TestToken__factory.createInterface(), wallet);
-  const mintFunction = await mUsdcContract.functions.mint(wallet.address, 100);
-
-  // Get the estimated gas cost of the mint function
-  const gasEstimate = await mUsdcContract.estimateGas.mint(wallet.address, 100);
-
-  // Build the transaction object
-  const tx = {
-    to: usdcAddress,
-    data: mintFunction,
-    gasLimit: 40000000000,
-    gasPrice: 40000000000,
-  };
-
-  // Sign and send the transaction
-  const signedTx = await wallet.signTransaction(tx);
-  const receipt = await provider.sendTransaction(signedTx);
-  await receipt.wait();
-  //   // const decimal = 10 ** (await mUsdcContract.decimals());
-  //   const amont = ethers.utils.parseEther('1000');
-  //   const tx = {
-  //     value: { address: wallet.address, amount: 10000 },
-  //     data: `mint(address, amount)`,
-  //     to: '0xA218A0EA9a888e3f6E2dfFdf4066885f596F07bF',
-  //     from: '0x5aB853A40b3b9A16891e8bc8e58730AE3Ec102b2',
-  //     gasLimit: 30000000000,
-  //   };
-  //   const tr = await wallet.sendTransaction(tx);
-  //   await tr.wait();
+  const tokenContract = new ethers.Contract(tokenAddress, TestToken__factory.createInterface(), wallet);
+  const mintTx = await tokenContract.mint(wallet.address, amount);
+  await mintTx.wait();
+  console.log('Mint tx hash: ', mintTx.hash);
+  const remainingAllowance = await tokenContract.allowance(wallet.address, bridgeAddress);
+  if (remainingAllowance === 0) {
+    const approveTx = await tokenContract.approve(bridgeAddress, ethers.constants.MaxUint256);
+    await approveTx.wait();
+    console.log('Approve tx hash: ', approveTx.hash);
+  }
 }
 
 main().catch((error) => {
