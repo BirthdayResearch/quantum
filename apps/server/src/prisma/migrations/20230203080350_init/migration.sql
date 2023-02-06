5837692a444f55056bc8401d139347b1a8c03ba2
Harsh <harshrathi.dev@gmail.com>
2023-02-06 10:11:33 +0530

    migration update

--- apps/server/src/prisma/migrations/20230203080350_init/migration.sql
+++ apps/server/src/prisma/migrations/20230203080350_init/migration.sql
@@ -7,10 +7,11 @@ CREATE TABLE "PathIndex" (
     "index" INTEGER NOT NULL,
     "address" TEXT NOT NULL,
     "network" "Network" NOT NULL,
+    "refundAddress" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "PathIndex_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "PathIndex_index_address_key" ON "PathIndex"("index", "network");
+CREATE UNIQUE INDEX "PathIndex_index_network_key" ON "PathIndex"("index", "network");
