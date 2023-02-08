e37ed7f4a761261335fb2db1accea63d47ac423e
JJ Adonis <work@josejai.me>
2023-02-08 16:43:59 +0800

    use uri() instead of ip()

--- apps/server/src/AppConfig.ts
+++ apps/server/src/AppConfig.ts
@@ -36,7 +36,7 @@ export type DeepPartial<T> = T extends object
 export type AppConfig = DeepPartial<ReturnType<typeof appConfig>>;
 
 export const ENV_VALIDATION_SCHEMA = Joi.object({
-  ETHEREUM_RPC_URL: Joi.string().ip(),
+  ETHEREUM_RPC_URL: Joi.string().uri(),
   DEFICHAIN_NETWORK: Joi.string(),
-  DEFICHAIN_WHALE_URL: Joi.string().ip(),
+  DEFICHAIN_WHALE_URL: Joi.string().uri(),
 });
