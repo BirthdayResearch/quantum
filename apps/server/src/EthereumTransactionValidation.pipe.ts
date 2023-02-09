dd7ff31818453cb609d352b25106a0327955f9e9
Greg G Tan <greggtan99@gmail.com>
2023-02-09 15:09:23 +0800

    fix(EthereumTransactionValidation pipe): implemented checks using regex instead.

--- apps/server/src/EthereumTransactionValidation.pipe.ts
+++ apps/server/src/EthereumTransactionValidation.pipe.ts
@@ -1,13 +1,10 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
-import { ethers } from 'ethers';
 
 @Injectable()
 export class EthereumTransactionValidationPipe implements PipeTransform<string> {
   async transform(transactionHash: string) {
-    const isTransactionHash =
-      ethers.utils.isHexString(transactionHash) &&
-      transactionHash.length === 66 &&
-      transactionHash.slice(0, 2) === '0x';
+    const regex = /^0x([A-Fa-f0-9]{64})$/;
+    const isTransactionHash = regex.test(transactionHash);
     if (isTransactionHash === false) {
       throw new BadRequestException(`Invalid Ethereum transaction hash: ${transactionHash}`);
     }
