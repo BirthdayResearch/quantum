05c0abed07e17df04b629c9ab1a7270e655b7db1
KevinK <kevin.kihata@gmail.com>
2023-02-14 18:09:05 +0800

    remove double stop container

--- apps/server/test-i9n/testing/HealthService.i9n.ts
+++ apps/server/test-i9n/testing/HealthService.i9n.ts
@@ -1,23 +1,18 @@
 import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@birthdayresearch/sticky-testcontainers';
 
-import { DeFiChainStubContainer, StartedDeFiChainStubContainer } from '../defichain/containers/DeFiChainStubContainer';
 import { BridgeServerTestingApp } from './BridgeServerTestingApp';
 import { buildTestConfig, TestingModule } from './TestingModule';
 
 describe('Health Service Test', () => {
   let testing: BridgeServerTestingApp;
   let startedPostgresContainer: StartedPostgreSqlContainer;
-  let defichain: StartedDeFiChainStubContainer;
 
   beforeAll(async () => {
     startedPostgresContainer = await new PostgreSqlContainer().start();
 
-    defichain = await new DeFiChainStubContainer().start();
-    const whaleURL = await defichain.getWhaleURL();
     testing = new BridgeServerTestingApp(
       TestingModule.register(
         buildTestConfig({
-          defichain: { whaleURL, key: StartedDeFiChainStubContainer.LOCAL_MNEMONIC },
           startedPostgresContainer,
         }),
       ),
@@ -28,8 +23,6 @@ describe('Health Service Test', () => {
 
   afterAll(async () => {
     await testing.stop();
-    await startedPostgresContainer.stop();
-    await defichain.stop();
   });
 
   it('Health check service should be ok', async () => {
