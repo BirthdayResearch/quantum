4890ba29673f9ebeb4fd8a2ea11ea98d7eb27b33
JJ Adonis <work@josejai.me>
2023-02-15 09:58:28 +0800

    update

--- .github/scripts/release-ecr-tags.js
+++ .github/scripts/release-ecr-tags.js
@@ -9,6 +9,12 @@ module.exports = ({ context }) => {
   if (context.eventName === 'release') {
     return getReleaseTag(context)
   }
+  if (isStaging(context) === true) {
+    return getMainTag(context)
+  }
+  if (isDev(context) === true) {
+    return getPullRequestTag(context)
+  }
   throw new Error('Release Violation: Could not determine the required release tags.')
 }
 
@@ -18,4 +24,20 @@ function getReleaseTag(context) {
     throw new Error(`Release Violation: Provided version '${semver}' is not valid semver.`)
   }
   return semver.replace('v','')
+}
+
+function getMainTag({ sha }) {
+  return `public.ecr.aws/w1x3w8l6/bridge-api:main,public.ecr.aws/w1x3w8l6/bridge-api:${sha}`
+}
+
+function getPullRequestTag({ payload: { number }, sha }) {
+  return `public.ecr.aws/w1x3w8l6/bridge-api:pr-${number},public.ecr.aws/w1x3w8l6/bridge-api:${sha}`
+}
+
+function isStaging(context) {
+  return context.eventName === 'push' && context.ref === 'refs/heads/main'
+}
+
+function isDev(context) {
+  return context.eventName === 'pull_request'
 }
