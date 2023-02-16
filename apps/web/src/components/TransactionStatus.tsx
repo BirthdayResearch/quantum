ed3fb8a4b8c216574ff0d41127dd476bbccc0445
joshua <josh_817@hotmail.com>
2023-02-16 14:32:33 +0800

    chore(ui-ux): update confirmed layout

--- apps/web/src/components/TransactionStatus.tsx
+++ apps/web/src/components/TransactionStatus.tsx
@@ -1,7 +1,10 @@
 import BigNumber from "bignumber.js";
 import { FiArrowUpRight } from "react-icons/fi";
+import { IoHelpCircle } from "react-icons/io5";
 import useResponsive from "../hooks/useResponsive";
 import { useContractContext } from "../layouts/contexts/ContractContext";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 export default function TransactionStatus({
   ethTxnStatus,
@@ -15,24 +18,48 @@ export default function TransactionStatus({
 }) {
   const { ExplorerURL } = useContractContext();
   const { isLg } = useResponsive();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (ethTxnStatus.isConfirmed) {
+      setTitle("Transaction confirmed");
+      setDescription("Expect to receive your tokens in your wallet shortly.");
+    } else {
+      setTitle("Processing transaction");
+      setDescription(
+        "Do not refresh, leave the browser, or close the tab until transaction is complete. Doing so may interrupt the transaction and cause loss of funds."
+      );
+    }
+  }, [ethTxnStatus.isConfirmed]);
+
   return (
-    <div className="w-full px-8 py-6 mb-6 text-dark-1000 dark-bg-gradient-1 rounded-xl border border-transparent">
-      <div className="leading-5 lg:text-xl lg:font-semibold">
-        Processing transaction
+    <div
+      className={clsx(
+        "w-full px-8 py-6 mb-6 text-dark-1000  rounded-xl border  bg-dark-100 ",
+        ethTxnStatus.isConfirmed
+          ? "border-dark-card-stroke"
+          : "dark-bg-gradient-1 border-transparent"
+      )}
+    >
+      <div className="leading-5 lg:text-xl lg:font-semibold">{title}</div>
+      <div className="pt-1 text-sm text-dark-700">{description}</div>
+      <div className="flex flex-row items-center mt-2 text-dark-900 text-xl font-bold ">
+        <a
+          className="flex flex-row items-center hover:opacity-70"
+          href={`${ExplorerURL}/tx/${txnHash}`}
+          target="_blank"
+        >
+          <FiArrowUpRight size={20} className="mr-2" />
+          View on Etherscan
+        </a>
+        {ethTxnStatus.isConfirmed && (
+          <a className="flex flex-row items-center hover:opacity-70 ml-5">
+            <IoHelpCircle size={20} className="mr-2" />
+            Help
+          </a>
+        )}
       </div>
-      <div className="pt-1 text-sm text-dark-700">
-        Do not refresh, leave the browser, or close the tab until transaction is
-        complete. Doing so may interrupt the transaction and cause loss of
-        funds.
-      </div>
-      <a
-        className="flex flex-row items-center mt-2 text-dark-900 text-xl font-bold hover:opacity-70"
-        href={`${ExplorerURL}/tx/${txnHash}`}
-        target="_blank"
-      >
-        <FiArrowUpRight size={20} className="mr-2" />
-        View on Etherscan
-      </a>
       <div>{`${
         new BigNumber(ethTxnStatus.numberOfConfirmations).isGreaterThan(65)
           ? "65"
