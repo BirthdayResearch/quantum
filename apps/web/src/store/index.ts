a8fa0f31807d51a5d56c8c66767d32047fff9bbd
JJ Adonis <work@josejai.me>
2023-02-18 15:50:13 +0800

    merge

--- apps/web/src/store/index.ts
+++ apps/web/src/store/index.ts
@@ -1,11 +1,14 @@
 import { statusWebsiteSlice } from "./status";
 import { bridgeApi } from "./defichain";
+import useWrappedMutation from "@hooks/useWrappedMutation";
+import useWrappedLazyQuery from "@hooks/useWrappedLazyQuery";
 
-const {
-  useGenerateAddressMutation,
-  useLazyVerifyQuery,
-  useGetAddressDetailMutation,
-} = bridgeApi;
+const useGenerateAddressMutation = () =>
+  useWrappedMutation(bridgeApi.useGenerateAddressMutation);
+const useLazyVerifyQuery = () =>
+  useWrappedLazyQuery(bridgeApi.useLazyVerifyQuery);
+const useGetAddressDetailMutation = () =>
+  useWrappedMutation(bridgeApi.useGetAddressDetailMutation);
 
 const { useGetBridgeStatusQuery } = statusWebsiteSlice;
 
