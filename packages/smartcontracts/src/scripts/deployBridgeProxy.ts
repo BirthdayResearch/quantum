9d18aa743ae6fa06cd71d87df1d89f35727d3744
Abhishek <abhishekkochar2@gmail.com>
2023-03-03 17:16:34 +1300

    Added script to verify contracts after the deployment

--- packages/smartcontracts/src/scripts/deployBridgeProxy.ts
+++ packages/smartcontracts/src/scripts/deployBridgeProxy.ts
@@ -1,6 +1,7 @@
-import { ethers, network } from 'hardhat';
+import { ethers } from 'hardhat';
 
 import { BridgeProxy, BridgeV1__factory } from '../generated';
+import { verify } from './utils/verify';
 
 const TRANSACTION_FEE = 10;
 
@@ -12,7 +13,6 @@ export async function deployBridgeProxy({
   txFeeAddress,
   flushReceiveAddress,
 }: InputsForInitialization): Promise<BridgeProxy> {
-  const { chainId } = network.config;
   const bridgeProxyContract = await ethers.getContractFactory('BridgeProxy');
   const encodedData = BridgeV1__factory.createInterface().encodeFunctionData('initialize', [
     // admin address, or timelock contract address
@@ -29,12 +29,8 @@ export async function deployBridgeProxy({
   const bridgeProxy = await bridgeProxyContract.deploy(bridgeV1Address, encodedData);
   await bridgeProxy.deployed();
   console.log('Proxy Address: ', bridgeProxy.address);
-  if (chainId !== 1337) {
-    console.log(
-      `To verify on Etherscan: npx hardhat verify --network goerli --contract contracts/BridgeProxy.sol:BridgeProxy ${bridgeProxy.address} ${bridgeV1Address} ${encodedData}`,
-    );
-  }
-
+  console.log('Verifying...');
+  await verify({ contractAddress: bridgeProxy.address, args: [bridgeV1Address, encodedData] });
   return bridgeProxy;
 }
 
