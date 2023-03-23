f44f08b526ec351544b2a98fbfe42fc2a82334b6
cuongquangnam <tccuong1999@gmail.com>
2023-03-23 23:20:53 +0800

    add another sanity check

--- apps/server/src/ethereum/services/EVMTransactionConfirmerService.ts
+++ apps/server/src/ethereum/services/EVMTransactionConfirmerService.ts
@@ -390,7 +390,10 @@ const decodeTxnData = (txDetail: ethers.providers.TransactionResponse) => {
   const iface = new ethers.utils.Interface(BridgeV1__factory.abi);
   const decodedData = iface.parseTransaction({ data: txDetail.data, value: txDetail.value });
   // Sanity check that the decoded function name is correct
-  if (decodedData.name !== 'bridgeToDeFiChain') {
+  if (
+    decodedData.name !== 'bridgeToDeFiChain' ||
+    decodedData.signature !== 'bridgeToDeFiChain(bytes,address,uint256)'
+  ) {
     throw Error('Invalid transactionHash');
   }
   const fragment = iface.getFunction(decodedData.name);
