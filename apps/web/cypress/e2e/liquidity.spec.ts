3aaf34e7c1c2a5af1acaad78ab25abb76cf74550
Mikhail <mykhailo@cakedefi.com>
2023-03-24 18:17:48 +0800

    prettify;

--- apps/web/cypress/e2e/liquidity.spec.ts
+++ apps/web/cypress/e2e/liquidity.spec.ts
@@ -1,3 +1,5 @@
+import BigNumber from "bignumber.js";
+
 const testNetUrl = "http://localhost:3001/liquidity?network=TestNet";
 
 const desktopViewPorts = ["macbook-15", "ipad-2"];
@@ -15,13 +17,13 @@ desktopViewPorts.forEach((viewPort) => {
       cy.viewport(<Cypress.ViewportPreset>viewPort);
     });
 
-    it("Should check Proof of Baking link", () => {
+    it("should check Proof of Baking link", () => {
       cy.get(".mt-1 > a")
         .should("have.attr", "href")
         .and("include", "https://defiscan.live/proof-of-backing");
     });
 
-    it("Should check that Addresses links are correct", () => {
+    it("should check that Addresses links are correct", () => {
       cy.findAllByText(dfcAddress)
         .should("have.length", 6)
         .and("have.attr", "href")
@@ -32,55 +34,55 @@ desktopViewPorts.forEach((viewPort) => {
         .and("include", "https://goerli.etherscan.io/address/" + evmAddress);
     });
 
-    it("Should check that liquidity values displayed correctly", () => {
+    it("should check that liquidity values displayed correctly", () => {
       cy.request(testNetBalancesUrl).then((response) => {
         cy.getLiquidityValueByIndex(0).should(
           "include",
-          Number(response.body.DFC.DFI).toFixed(8)
+          BigNumber(response.body.DFC.DFI).toFixed(8)
         );
         cy.getLiquidityValueByIndex(1).should(
           "include",
-          Number(response.body.EVM.DFI).toFixed(8)
+          BigNumber(response.body.EVM.DFI).toFixed(8)
         );
         cy.getLiquidityValueByIndex(2).should(
           "include",
-          Number(response.body.DFC.BTC).toFixed(8)
+          BigNumber(response.body.DFC.BTC).toFixed(8)
         );
         cy.getLiquidityValueByIndex(3).should(
           "include",
-          Number(response.body.EVM.WBTC).toFixed(8)
+          BigNumber(response.body.EVM.WBTC).toFixed(8)
         );
         cy.getLiquidityValueByIndex(4).should(
           "include",
-          Number(response.body.DFC.ETH).toFixed(8)
+          BigNumber(response.body.DFC.ETH).toFixed(8)
         );
         cy.getLiquidityValueByIndex(5).should(
           "include",
-          Number(response.body.EVM.ETH).toFixed(8)
+          BigNumber(response.body.EVM.ETH).toFixed(8)
         );
         cy.getLiquidityValueByIndex(6).should(
           "include",
-          Number(response.body.DFC.USDT).toFixed(8)
+          BigNumber(response.body.DFC.USDT).toFixed(8)
         );
         cy.getLiquidityValueByIndex(7).should(
           "include",
-          Number(response.body.EVM.USDT).toFixed(8)
+          BigNumber(response.body.EVM.USDT).toFixed(8)
         );
         cy.getLiquidityValueByIndex(8).should(
           "include",
-          Number(response.body.DFC.USDC).toFixed(8)
+          BigNumber(response.body.DFC.USDC).toFixed(8)
         );
         cy.getLiquidityValueByIndex(9).should(
           "include",
-          Number(response.body.EVM.USDC).toFixed(8)
+          BigNumber(response.body.EVM.USDC).toFixed(8)
         );
         cy.getLiquidityValueByIndex(10).should(
           "include",
-          Number(response.body.DFC.EUROC).toFixed(8)
+          BigNumber(response.body.DFC.EUROC).toFixed(8)
         );
         cy.getLiquidityValueByIndex(11).should(
           "include",
-          Number(response.body.EVM.EUROC).toFixed(8)
+          BigNumber(response.body.EVM.EUROC).toFixed(8)
         );
       });
     });
@@ -95,7 +97,7 @@ context(`Liquidity page on ${mobileViewPort}`, () => {
     cy.viewport(<Cypress.ViewportPreset>mobileViewPort);
   });
 
-  it("Should check navigation between Bridge and Liquidity pages", () => {
+  it("should check navigation between Bridge and Liquidity pages", () => {
     cy.get(".flex-col  .px-5 > div.p-1 > a")
       .first()
       .should("be.visible")
@@ -107,7 +109,7 @@ context(`Liquidity page on ${mobileViewPort}`, () => {
       .click();
   });
 
-  it("Should check responsive design for the Liquidity table elements", () => {
+  it("should check responsive design for the Liquidity table elements", () => {
     cy.get(".space-y-3 > .border-gradient-6 svg[stroke='currentColor']").should(
       "have.length",
       6
