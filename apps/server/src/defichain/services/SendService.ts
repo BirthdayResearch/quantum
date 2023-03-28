f80dac8686fe19a6b688140c812e7321439a5f48
Keng Ye <40191153+kyleleow@users.noreply.github.com>
2023-03-28 17:18:37 +0800

    fix(server): add dfi balance checking logic to allocate fund (#849)
    * fix: deduct send amount when verifying dfi balance
    
    * chore: add dfi balance checking logic to allocate fund
    
    Keeping the current one at send but without checking for reserved utxo
    
    * chore: increase hot wallet balance in test
    
    * Revert "chore: increase hot wallet balance in test"
    
    This reverts commit d4ffb5cd216824c44b60343dd26e8ba9c44e80ab.

--- apps/server/src/defichain/services/SendService.ts
+++ apps/server/src/defichain/services/SendService.ts
@@ -1,6 +1,5 @@
 import { TransactionSegWit } from '@defichain/jellyfish-transaction';
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import BigNumber from 'bignumber.js';
 
 import { WhaleWalletProvider } from '../providers/WhaleWalletProvider';
@@ -12,7 +11,6 @@ export class SendService {
 
   constructor(
     private readonly transactionService: DeFiChainTransactionService,
-    private readonly configService: ConfigService,
     private readonly whaleWalletProvider: WhaleWalletProvider,
   ) {
     this.logger = new Logger(SendService.name);
@@ -23,7 +21,7 @@ export class SendService {
       let signed: TransactionSegWit;
       // To be able to send UTXO DFI
       if (token.symbol === 'DFI') {
-        this.verifyDFIBalance();
+        this.verifyDFIBalance(token.amount);
         signed = await builder.utxo.send(token.amount, to, from);
       } else {
         // Rest of dTokens to use this tx type
@@ -50,12 +48,9 @@ export class SendService {
     return this.transactionService.broadcastTransaction(signedTX, 0);
   }
 
-  private async verifyDFIBalance(): Promise<void> {
-    const hotWallet = this.whaleWalletProvider.getHotWallet();
-    const hotWalletAddress = await hotWallet.getAddress();
-    const balance = await hotWallet.client.address.getBalance(hotWalletAddress);
-    const dfcReservedAmt = this.configService.getOrThrow('defichain.dfcReservedAmt', 0);
-    const DFIBalance = BigNumber.max(0, new BigNumber(balance).minus(dfcReservedAmt));
+  private async verifyDFIBalance(amountToSend: BigNumber): Promise<void> {
+    const balance = await this.whaleWalletProvider.getHotWalletBalance();
+    const DFIBalance = BigNumber.max(0, new BigNumber(balance).minus(amountToSend));
     if (DFIBalance.isLessThanOrEqualTo(0) || DFIBalance.isNaN()) {
       this.logger.log(`[Sending UTXO] Failed to send because insufficient DFI UTXO in hot wallet`);
       throw new BadRequestException('Insufficient DFI liquidity');
