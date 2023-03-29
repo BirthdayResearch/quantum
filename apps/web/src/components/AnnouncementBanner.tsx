eef8fa5ca1909e2fc6a73ae5e0ec6dd134e30e30
Lyka Labrada <lykalabrada@gmail.com>
2023-03-29 22:17:09 +0800

    feat(ui-ux): get app version from store

--- apps/web/src/components/AnnouncementBanner.tsx
+++ apps/web/src/components/AnnouncementBanner.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { FiArrowUpRight } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
 import { satisfies } from "semver";
-import { useLazyBridgeAnnouncements } from "store";
+import { RootState } from "@store/reducers/rootReducer";
+import { selectVersion } from "@store/slices/versionSlice";
+import { useLazyBridgeAnnouncements } from "@store/index";
 import { AnnouncementData } from "types";
 
 export default function AnnouncementBanner(): JSX.Element {
+  const appVersion = useSelector((state: RootState) => selectVersion(state));
   const [trigger] = useLazyBridgeAnnouncements();
   const [announcement, setAnnouncement] = useState<{
     content: string;
@@ -15,8 +19,7 @@ export default function AnnouncementBanner(): JSX.Element {
   async function getAnnouncements() {
     const { data } = await trigger({});
     const announce: AnnouncementData = data.find(({ version }) =>
-      // TODO: Get the proper app version
-      satisfies("1.6.1", version)
+      satisfies(appVersion, version)
     );
     if (announce) {
       setAnnouncement({ content: announce.lang.en, url: announce.url });
@@ -28,7 +31,6 @@ export default function AnnouncementBanner(): JSX.Element {
   }, []);
 
   return announcement ? (
-    // TODO: Handle ipad and mobile view
     <div className="flex flex-row justify-between items-center py-[18px] px-6 md:px-10 lg:px-[120px] bg-dark-gradient-4">
       <div className="flex items-center text-dark-00 text-xs lg:text-sm">
         <span>{announcement.content}</span>
