fc49341495b9ba8031eda924689713bd18e3713e
Mikhail <mykhailo@cakedefi.com>
2023-03-31 18:35:46 +0800

    refactored; add network configs;

--- apps/web/cypress/e2e/liquidity.spec.ts
+++ apps/web/cypress/e2e/liquidity.spec.ts
@@ -1,145 +1,91 @@
 import BigNumber from "bignumber.js";
-
-const urls = ["/liquidity?network=TestNet", "/liquidity?network=MainNet"];
+import { networkConfigs } from "../fixtures/networks.config";
 
 const desktopViewPort = "macbook-15";
 const mobileViewPort = "iphone-x";
 
-urls.forEach((url) => {
-  context(`Liquidity page on ${desktopViewPort} at the ${url}`, () => {
-    const dfcAddressTn = "tf1qsckyp02vdzaf95cjl5dr95n8stcalze0pfswcp";
-    const dfcAddressMn = "df1qgq0rjw09hr6vr7sny2m55hkr5qgze5l9hcm0lg";
-    const evmAddressTn = "0x96E5E1d6377ffA08B9c08B066f430e33e3c4C9ef";
-    const evmAddressMn = "0x54346d39976629b65ba54eac1c9ef0af3be1921b";
+const baseUrl = "/liquidity?network=";
 
-    const testNetBalancesUrl = "https://testnet.api.quantumbridge.app/balances";
-    const mainNetBalancesUrl = "https://api.quantumbridge.app/balances";
+const evmSymbols = ["DFI", "WBTC", "ETH", "USDT", "USDC", "EUROC"];
+const dfcSymbols = ["dEUROC", "dUSDC", "dUSDT", "dETH", "dBTC", "DFI"];
 
-    beforeEach(() => {
-      cy.visit(url);
-      cy.viewport(desktopViewPort);
-    });
+networkConfigs.forEach((networkItem) => {
+  context(
+    `Liquidity page on ${desktopViewPort} at the ${networkItem.network}`,
+    () => {
+      beforeEach(() => {
+        cy.visit(baseUrl + networkItem.network);
+        cy.viewport(desktopViewPort);
+      });
 
-    it("should check Proof of Backing link", () => {
-      cy.findByTestId("POB.Url")
-        .should("have.attr", "href")
-        .and("include", "https://defiscan.live/proof-of-backing"); //TODO: add check for the mainnet by default after fix
-    });
+      it("should check Proof of Backing link", () => {
+        cy.findByTestId("POB.Url")
+          .should("have.attr", "href")
+          .and("include", "https://defiscan.live/proof-of-backing"); //TODO: add check for the mainnet by default after fix
+      });
 
-    it("should check that Addresses links are correct", () => {
-      if (url.includes("TestNet")) {
-        cy.findAllByText(dfcAddressTn)
-          .should("have.length", 6)
-          .and("have.attr", "href")
-          .and("include", "https://defiscan.live/address/" + dfcAddressTn);
-        cy.findAllByText(evmAddressTn)
-          .should("have.length", 6)
-          .and("have.attr", "href")
-          .and(
-            "include",
-            "https://goerli.etherscan.io/address/" + evmAddressTn
-          );
-      } else {
-        cy.findAllByText(dfcAddressMn)
-          .should("have.length", 6)
-          .and("have.attr", "href")
-          .and("include", "https://defiscan.live/address/" + dfcAddressMn);
-        cy.findAllByText(evmAddressMn)
-          .should("have.length", 6)
-          .and("have.attr", "href")
-          .and("include", "https://etherscan.io/address/" + evmAddressMn);
-      }
-    });
+      it("should check that Addresses links are correct", () => {
+        evmSymbols.forEach((symbol) => {
+          cy.findByTestId(symbol + "-Ethereum-address")
+            .and("have.attr", "href")
+            .and("include", networkItem.evmAddress);
+        });
+        dfcSymbols.forEach((symbol) => {
+          cy.findByTestId(symbol + "-DeFiChain-address")
+            .and("have.attr", "href")
+            .and("include", networkItem.dfcAddress);
+        });
+      });
 
-    it("should check that liquidity values displayed correctly", () => {
-      if (url.includes("TestNet")) {
-        cy.request(testNetBalancesUrl).then((response) => {
-          cy.getLiquidityBySymbolChain("DFI", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.DFI).toFixed(8)
+      it("should check that liquidity values displayed correctly", () => {
+        cy.request(networkItem.balancesUrl).then((response) => {
+          dfcSymbols.reverse().forEach((symbol) => {
+            const apiSymbol = symbol.replaceAll("d", "");
+            cy.getLiquidityBySymbolChain(symbol, "DeFiChain").should(
+              "include",
+              BigNumber(response.body.DFC[apiSymbol]).toFixed(8)
+            );
+          });
+          evmSymbols.forEach((symbol) => {
+            cy.getLiquidityBySymbolChain(symbol, "Ethereum").should(
+              "include",
+              BigNumber(response.body.EVM[symbol]).toFixed(8)
+            );
+          });
+        });
+      });
+
+      context(`Liquidity page on ${mobileViewPort}`, () => {
+        beforeEach(() => {
+          cy.visit(baseUrl + networkItem.network);
+          cy.viewport(mobileViewPort);
+        });
+
+        it("should check navigation between Bridge and Liquidity pages", () => {
+          cy.get("a[data-testid='Navigation.Bridge']")
+            .last()
+            .should("be.visible")
+            .click();
+          cy.url().should("not.contain", "liquidity");
+          cy.get("a[data-testid='Navigation.Liquidity']")
+            .last()
+            .should("be.visible")
+            .click();
+          cy.url().should("contain", "liquidity");
+        });
+
+        it("should check responsive design for the Liquidity table elements", () => {
+          cy.get("svg[data-testid='Mobile.Card.DropdownArrow']").should(
+            "have.length",
+            6
           );
-          cy.getLiquidityBySymbolChain("dBTC", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.BTC).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("dETH", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.ETH).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("dUSDT", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.USDT).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("dUSDC", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.USDC).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("dEUROC", "DeFiChain").should(
-            "include",
-            BigNumber(response.body.DFC.EUROC).toFixed(8)
+          cy.viewport(desktopViewPort);
+          cy.get("svg[data-testid='Mobile.Card.DropdownArrow']").should(
+            "have.length",
+            0
           );
         });
-      } else {
-        cy.request(mainNetBalancesUrl).then((response) => {
-          cy.getLiquidityBySymbolChain("DFI", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.DFI).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("WBTC", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.WBTC).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("ETH", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.ETH).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("USDT", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.USDT).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("USDC", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.USDC).toFixed(8)
-          );
-          cy.getLiquidityBySymbolChain("EUROC", "Ethereum").should(
-            "include",
-            BigNumber(response.body.EVM.EUROC).toFixed(8)
-          );
-        });
-      }
-    });
-  });
-});
-
-context(`Liquidity page on ${mobileViewPort}`, () => {
-  beforeEach(() => {
-    cy.visit(urls[1]);
-    cy.viewport(mobileViewPort);
-  });
-
-  it("should check navigation between Bridge and Liquidity pages", () => {
-    cy.get("a[data-testid='Navigation.Bridge']")
-      .last()
-      .should("be.visible")
-      .click();
-    cy.url().should("not.contain", "liquidity");
-    cy.get("a[data-testid='Navigation.Liquidity']")
-      .last()
-      .should("be.visible")
-      .click();
-    cy.url().should("contain", "liquidity");
-  });
-
-  it("should check responsive design for the Liquidity table elements", () => {
-    cy.get("svg[data-testid='Mobile.Card.DropdownArrow']").should(
-      "have.length",
-      6
-    );
-
-    cy.viewport(desktopViewPort);
-    cy.get("svg[data-testid='Mobile.Card.DropdownArrow']").should(
-      "have.length",
-      0
-    );
-  });
+      });
+    }
+  );
 });
