ed67bd6b0c3fbcd05f502202375747be99075b6d
joshua <josh_817@hotmail.com>
2023-04-11 10:31:55 +0800

    chore(e2e): check metamask balance after dfc to eth bridge

---
+++ apps/web/cypress/support/ethUtils.ts
@@ -0,0 +1,365 @@
+import { BigNumber as BigEther, ethers } from "ethers";
+
+// TODO: Use the generated Abi directly
+const TestTokenAbi = {
+  abi: [
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "a",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "b",
+          type: "string",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+      ],
+      name: "allowance",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "approve",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "account",
+          type: "address",
+        },
+      ],
+      name: "balanceOf",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "decimals",
+      outputs: [
+        {
+          internalType: "uint8",
+          name: "",
+          type: "uint8",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "subtractedValue",
+          type: "uint256",
+        },
+      ],
+      name: "decreaseAllowance",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "addedValue",
+          type: "uint256",
+        },
+      ],
+      name: "increaseAllowance",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "_to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "_amount",
+          type: "uint256",
+        },
+      ],
+      name: "mint",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "transfer",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "transferFrom",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ],
+};
+
+export function toWei(amount: string): BigEther {
+  return ethers.utils.parseEther(amount);
+}
+
+export async function balanceOf(
+  address: string,
+  tokenAddress: string
+): Promise<BigEther> {
+  const ethWalletPrivateKey =
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const rpcURL = "http://localhost:8545";
+  const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+  const contractSigner = new ethers.Wallet(ethWalletPrivateKey, provider);
+
+  const tokenContract = new ethers.Contract(
+    tokenAddress,
+    TestTokenAbi.abi,
+    contractSigner
+  );
+
+  return await tokenContract.balanceOf(address);
+}
+
+async function mintToken(symbol: string, amount: string) {
+  const account0Address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  const MUSDTContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"; //MUSDT
+
+  let tokenAddress = "";
+  if (symbol === "USDT") {
+    tokenAddress = MUSDTContractAddress;
+  }
+
+  const ethWalletPrivateKey =
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const rpcURL = "http://localhost:8545";
+  const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+  const contractSigner = new ethers.Wallet(ethWalletPrivateKey, provider);
+
+  // Creating a signing account from a private key
+
+  const MUSDTContract = new ethers.Contract(
+    MUSDTContractAddress,
+    TestTokenAbi.abi,
+    contractSigner
+  );
+
+  await MUSDTContract.mint(account0Address, toWei(amount));
+
+  // Code below, just for testing
+  // const balance = await MUSDTContract.balanceOf(account0Address);
+  // const assetDecimalPlaces = await MUSDTContract.decimals();
+  // cy.log(ethers.utils.formatUnits(balance, assetDecimalPlaces));
+}
