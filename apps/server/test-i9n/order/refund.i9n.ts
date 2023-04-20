5cabe4f67cf5b995d67defbb74685c1f8a7afde1
veralygit <57183851+veralygit@users.noreply.github.com>
2023-04-20 17:46:14 +0800

    add hardhat container

--- apps/server/test-i9n/order/refund.i9n.ts
+++ apps/server/test-i9n/order/refund.i9n.ts
@@ -1,6 +1,15 @@
 import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@birthdayresearch/sticky-testcontainers';
+import {
+  BridgeV1,
+  HardhatNetwork,
+  HardhatNetworkContainer,
+  StartedHardhatNetworkContainer,
+  TestToken,
+} from 'smartcontracts';
 
 import { PrismaService } from '../../src/PrismaService';
+import { StartedDeFiChainStubContainer } from '../defichain/containers/DeFiChainStubContainer';
+import { BridgeContractFixture } from '../testing/BridgeContractFixture';
 import { BridgeServerTestingApp } from '../testing/BridgeServerTestingApp';
 import { buildTestConfig, TestingModule } from '../testing/TestingModule';
 
@@ -9,17 +18,50 @@ let testing: BridgeServerTestingApp;
 
 describe('Request Refund Testing', () => {
   let prismaService: PrismaService;
+  let startedHardhatContainer: StartedHardhatNetworkContainer;
+  let hardhatNetwork: HardhatNetwork;
+  let bridgeContract: BridgeV1;
+  let bridgeContractFixture: BridgeContractFixture;
+  let musdcContract: TestToken;
+
+  const validTxnHash = '0x09bf1c99b2383677993378227105c938d4fc2a2a8998d6cd35fccd75ee5b3834';
 
   beforeAll(async () => {
     startedPostgresContainer = await new PostgreSqlContainer().start();
-    const dynamicModule = TestingModule.register(buildTestConfig({ startedPostgresContainer }));
+    startedHardhatContainer = await new HardhatNetworkContainer().start();
+    hardhatNetwork = await startedHardhatContainer.ready();
+
+    bridgeContractFixture = new BridgeContractFixture(hardhatNetwork);
+    await bridgeContractFixture.setup();
+
+    ({ bridgeProxy: bridgeContract, musdc: musdcContract } =
+      bridgeContractFixture.contractsWithAdminAndOperationalSigner);
+
+    const dynamicModule = TestingModule.register(
+      buildTestConfig({
+        defichain: { key: StartedDeFiChainStubContainer.LOCAL_MNEMONIC },
+        startedHardhatContainer,
+        testnet: { bridgeContractAddress: bridgeContract.address },
+        startedPostgresContainer,
+        usdcAddress: musdcContract.address,
+      }),
+    );
+
     testing = new BridgeServerTestingApp(dynamicModule);
     const app = await testing.start();
     prismaService = app.get<PrismaService>(PrismaService);
+  });
 
-    prismaService.ethereumOrders.create({
+  afterAll(async () => {
+    await prismaService.ethereumOrders.deleteMany({});
+    await startedPostgresContainer.stop();
+    await testing.stop();
+  });
+
+  it('Should be able to update order status to REFUND_REQUESTED', async () => {
+    const order = await prismaService.ethereumOrders.create({
       data: {
-        transactionHash: '0x09bf1c99b2383677993378227105c938d4fc2a2a8998d6cd35fccd75ee5b3834',
+        transactionHash: validTxnHash,
         ethereumStatus: 'NOT_CONFIRMED',
         status: 'DRAFT',
         createdAt: '2023-04-20T06:14:43.847Z',
@@ -30,23 +72,28 @@ describe('Request Refund Testing', () => {
         expiryDate: '1970-01-01T00:00:00.000Z',
       },
     });
-  });
 
-  afterAll(async () => {
-    await prismaService.ethereumOrders.deleteMany({});
-    await startedPostgresContainer.stop();
-    await testing.stop();
-  });
+    expect(order).toEqual({
+      id: BigInt(1),
+      transactionHash: validTxnHash,
+      ethereumStatus: 'NOT_CONFIRMED',
+      status: 'DRAFT',
+      createdAt: new Date('2023-04-20T06:14:43.847Z'),
+      updatedAt: new Date('2023-04-20T06:28:17.185Z'),
+      amount: null,
+      tokenSymbol: null,
+      defichainAddress: '',
+      expiryDate: new Date('1970-01-01T00:00:00.000Z'),
+    });
 
-  it('Should be able to update order status to REFUND_REQUESTED', async () => {
     const resp = await testing.inject({
       method: 'PUT',
-      url: `/ethereum/order/0x09bf1c99b2383677993378227105c938d4fc2a2a8998d6cd35fccd75ee5b3834/refund`,
+      url: `/ethereum/order/${validTxnHash}/refund`,
     });
 
     expect(resp.body).toStrictEqual({
       id: '1',
-      transactionHash: '0x09bf1c99b2383677993378227105c938d4fc2a2a8998d6cd35fccd75ee5b3834',
+      transactionHash: validTxnHash,
       ethereumStatus: 'NOT_CONFIRMED',
       status: 'REFUND_REQUESTED',
       createdAt: '2023-04-20T06:14:43.847Z',
@@ -64,7 +111,8 @@ describe('Request Refund Testing', () => {
       url: `/ethereum/order/0x09bf1c99b2383677993378227105c938d4fc2a2a8998d6cd35fccd75ee5b3835/refund`,
     });
 
-    expect(resp.body).toHaveProperty(
+    expect(resp.statusCode).toEqual(500);
+    expect(JSON.parse(resp.body).error).toEqual(
       "API call for refund was unsuccessful: Cannot read properties of null (reading 'data')",
     );
   });
@@ -75,8 +123,6 @@ describe('Request Refund Testing', () => {
       url: `/ethereum/order/1234/refund`,
     });
 
-    expect(resp.body).toEqual(
-      '{"statusCode":400,"message":"Invalid Ethereum transaction hash: 1234","error":"Bad Request"}',
-    );
+    expect(JSON.parse(resp.body).message).toEqual('Invalid Ethereum transaction hash: 1234');
   });
 });
