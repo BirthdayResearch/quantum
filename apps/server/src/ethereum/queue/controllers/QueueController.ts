db2b83ddd254d42340eb881db34db7b94ff41971
veralygit <57183851+veralygit@users.noreply.github.com>
2023-05-05 09:56:18 +0800

    feat(server): add get and list endpoint for ethQueue table (#966)
    * feature(db): update order to queue naming convention
    
    * updte fields to be optional
    
    * update comment
    
    * update comment
    
    * feat(server): add get and list endpoint for ethOrders table
    
    * add additional comments and add Queue service and Queue Controller
    
    * update foreignkey in adminethereumqueue
    
    * rename to queue
    
    * add REFUND_PROCESSED for queue status
    
    * update EthereumQueue table
    
    * code cleanup
    
    * add tests and update to use latest queue table
    
    * remove BotStatus on AdminEthereumQueue table
    
    * fix typo in comment for i9n test
    
    * update to add additional details to controller and update Queue module
    
    * update to return empty list if queueList length is 0 or undefined
    
    * update to return count of queue instead of lastCursor
    
    * update paginationSize
    
    * add throttling and update to use include for prisma select
    
    * update rate limit for get
    
    * add test cases
    
    * add throttle check test case
    
    * add additional testcases and update redundant code
    
    * commit QueueService updated codes
    
    * update to wrap empty data set with ApiPagedResponse when queueList is empty
    
    * update to set default value at ApiQuery
    
    * update pipe logic
    
    * standardize string casting
    
    * update enum validation logic
    
    * add additional test case to test for size limit
    
    * fix typo in testcase comment
    
    * update test cases
    
    * update test cases and conditional logic to remove duplicate codes
    
    ---------
    
    Co-authored-by: chloe <44501120+chloezxyy@users.noreply.github.com>

---
+++ apps/server/src/ethereum/queue/controllers/QueueController.ts
@@ -0,0 +1,53 @@
+import { Controller, Get, Param, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
+import { QueueStatus } from '@prisma/client';
+
+import { ApiPagedResponse } from '../../../pagination/ApiPagedResponse';
+import { PaginationQuery } from '../../../pagination/ApiQuery';
+import { EnumValidationPipe } from '../../../pipes/EnumValidation.pipe';
+import { EthereumQueueMultiStatusPipe } from '../../../pipes/EthereumQueueStatus.pipe';
+import { EthereumTransactionValidationPipe } from '../../../pipes/EthereumTransactionValidation.pipe';
+import { OrderBy, Queue } from '../model/Queue';
+import { QueueService } from '../services/QueueService';
+
+@Controller()
+export class QueueController {
+  constructor(private readonly queueService: QueueService) {}
+
+  /**
+   * Get information about a queue with given transactionHash.
+   *
+   * @param {string} transactionHash transactionHash
+   * @param {QueueStatus} [status=QueueStatus] status of queue
+   * @returns {Promise<Queue>}
+   */
+  @Get(':transactionHash')
+  @Throttle(5, 60)
+  async getQueue(
+    @Param('transactionHash', new EthereumTransactionValidationPipe()) transactionHash: string,
+    @Query('status', new EnumValidationPipe(QueueStatus))
+    status?: QueueStatus,
+  ): Promise<Queue> {
+    return this.queueService.getQueue(transactionHash, status);
+  }
+
+  /**
+   * Return paginated queue list.
+   *
+   * @param {QueueStatus[]} status accepts multiple status filters split by ',' eg: status=QueueStatus.IN_PROGRESS,QueueStatus.COMPLETED
+   * @param {PaginationQuery} query pagination query
+   * @param {OrderBy} [orderBy=OrderBy.ASC] default value of list orderBy is ASC
+   * @returns {Promise<ApiPagedResponse<Queue> | []>}
+   */
+  @Get('list')
+  @Throttle(20, 60)
+  @UsePipes(new ValidationPipe({ skipMissingProperties: true })) // this is required for validating and transforming the incoming request data based on the validation decorators for Pagination Query
+  async listQueue(
+    @Query('status', new EthereumQueueMultiStatusPipe(QueueStatus)) status: QueueStatus[],
+    @Query() query?: PaginationQuery,
+    @Query('orderBy', new EnumValidationPipe(OrderBy, OrderBy.ASC))
+    orderBy?: OrderBy,
+  ): Promise<ApiPagedResponse<Queue>> {
+    return this.queueService.listQueue(query, orderBy, status);
+  }
+}
