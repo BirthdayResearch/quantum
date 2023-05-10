f32d6fbecaff1b83abbc8107474f925087058ca9
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-05-10 17:24:14 +0700

    add copy and open function

--- apps/web/src/components/erc-transfer/QueueTransactionModal.tsx
+++ apps/web/src/components/erc-transfer/QueueTransactionModal.tsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from "react";
 import ActionButton from "@components/commons/ActionButton";
 import Modal from "@components/commons/Modal";
 import SearchQueuedTransactionButton from "@components/SearchQueuedTransactionButton";
+import { useContractContext } from "@contexts/ContractContext";
+import { SuccessCopy } from "@components/QrAddress";
+import useCopyToClipboard from "@hooks/useCopyToClipboard";
 
 export interface ModalConfigType {
   title: string;
   message: string;
 
   buttonLabel: string;
-  // contractType: ContractType; // TODO: handle type when new SC is merged
   onClose: () => void;
+
+  transactionHash: string;
+  destinationAddress: string;
+  amount: string;
+  symbol: string;
 }
 
 export default function QueueTransactionModal({
@@ -17,19 +24,34 @@ export default function QueueTransactionModal({
   message,
   buttonLabel,
   onClose,
+  transactionHash,
+  destinationAddress,
+  amount,
+  symbol,
 }: ModalConfigType) {
-  const [copiedFromClipboard, setCopiedFromClipboard] =
-    useState<boolean>(false);
+  const { ExplorerURL } = useContractContext();
+
+  const [showSuccessCopy, setShowSuccessCopy] = useState(false);
+  const { copy } = useCopyToClipboard();
+
+  const handleOnCopy = (text) => {
+    copy(text);
+    setShowSuccessCopy(true);
+  };
 
   useEffect(() => {
-    if (copiedFromClipboard) {
-      setTimeout(() => setCopiedFromClipboard(false), 2000);
+    if (showSuccessCopy) {
+      setTimeout(() => setShowSuccessCopy(false), 2000);
     }
-  }, [copiedFromClipboard]);
+  }, [showSuccessCopy]);
 
   return (
     <Modal isOpen onClose={onClose}>
       <div className="flex flex-col md:mb-[46px] lg:mb-[80px] w-full justify-between h-full">
+        <SuccessCopy
+          containerClass="m-auto right-0 left-0 top-2"
+          show={showSuccessCopy}
+        />
         <div>
           <div className="flex flex-col mb-6 md:mb-5 w-full md:px-6 md:items-center pb-[32px] md:pb-9 border-b border-dark-300 border-opacity-50">
             <SearchQueuedTransactionButton
@@ -54,14 +76,16 @@ export default function QueueTransactionModal({
               <div className="text-dark-700 text-sm lg:text-base leading-5">
                 Amount to receive
               </div>
-              <div className="text-dark-1000 text-right">150 dBTC</div>
+              <div className="text-dark-1000 text-right">
+                {amount} {symbol}
+              </div>
             </div>
             <div className="flex flex-row justify-between">
               <div className="text-dark-700 text-sm lg:text-base leading-5">
                 Destination address
               </div>
               <div className="text-dark-1000 w-6/12 break-words text-right">
-                dfa1123ZAaklz9012ZLasdalax1
+                {destinationAddress}
               </div>
             </div>
             <div className="flex flex-row justify-between">
@@ -71,7 +95,7 @@ export default function QueueTransactionModal({
                 </div>
               </div>
               <div className="text-dark-1000 text-right w-5/12 lg:w-[220px] break-all">
-                0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb
+                {transactionHash}
               </div>
             </div>
           </div>
@@ -83,14 +107,14 @@ export default function QueueTransactionModal({
             label={buttonLabel}
             customStyle="bg-dark-1000 text-sm lg:text-lg lg:px-[72px] max-w-[418px] min-h-[44px] max-h-[56px]"
             customIconStyle="ml-2"
-            // onClick={() => {})}
+            onClick={() => window.open(`${ExplorerURL}/tx/${transactionHash}`)}
           />
           <ActionButton
             variant="secondary"
             label="Copy transaction hash"
             customStyle="text-sm lg:text-lg lg:px-[72px] max-w-[418px] pt-[24px] pb-3.5"
             responsiveStyle=""
-            // onClick={() => {}}
+            onClick={() => handleOnCopy(transactionHash)}
           />
         </div>
       </div>
