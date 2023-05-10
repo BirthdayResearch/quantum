f0d54c0e9342cc7c1d0e33e9d3e4fccec554148e
G9000 <leo.caesar@live.com>
2023-05-10 13:08:00 +0800

    cleanup

--- apps/server/src/defichain/queue/QueueModule.ts
+++ apps/server/src/defichain/queue/QueueModule.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 
-import { EthersModule } from '../../modules/EthersModule';
 import { PrismaService } from '../../PrismaService';
+import { WhaleApiClientProvider } from '../providers/WhaleApiClientProvider';
+import { WhaleWalletProvider } from '../providers/WhaleWalletProvider';
+import { DeFiChainTransactionService } from '../services/DeFiChainTransactionService';
+import { WhaleApiService } from '../services/WhaleApiService';
 import { QueueController } from './controllers/QueueController';
 import { QueueService } from './services/QueueService';
 
 @Module({
-  providers: [PrismaService, QueueService],
+  providers: [
+    PrismaService,
+    QueueService,
+    DeFiChainTransactionService,
+    WhaleWalletProvider,
+    WhaleApiClientProvider,
+    WhaleApiService,
+  ],
   controllers: [QueueController],
-  imports: [EthersModule],
 })
 export class QueueModule {}
