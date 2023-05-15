d8863603b2d38844afd82462ad96e66e592daef8
cuongquangnam <tccuong1999@gmail.com>
2023-05-15 17:46:05 +0800

    add deployment scripts

---
+++ packages/smartcontracts-queue/src/scripts/testnetDeploymentScripts.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'hardhat';
+
+import { bridgeImplementation } from './deployBridgeImplementation';
+import { deployBridgeProxy } from './deployBridgeProxy';
+import { deployERC20 } from './deployERC20';
+
+async function main() {
+  const bridgeQueue = await bridgeImplementation();
+  const accounts = await ethers.provider.listAccounts();
+
+  const bridgeQueueProxy = await deployBridgeProxy({
+    timelockContractAddress: accounts[0],
+    coldWalletAddress: accounts[0],
+    fee: '0',
+    communityWalletAddress: accounts[0],
+    bridgeQueueAddress: bridgeQueue.address,
+  });
+  const firstToken = await deployERC20({ name: 'MockUSDC', symbol: 'MUSDC' });
+  const secondToken = await deployERC20({ name: 'MockUSDT', symbol: 'MUSDT' });
+  const bridgeQueueFactory = await ethers.getContractFactory('BridgeQueue');
+  const bridgeQueueProxyAttached = bridgeQueueFactory.attach(bridgeQueueProxy.address);
+  // add support for tokens
+  await bridgeQueueProxyAttached.addSupportedToken(firstToken.address);
+  await bridgeQueueProxyAttached.addSupportedToken(secondToken.address);
+  await bridgeQueueProxyAttached.addSupportedToken(ethers.constants.AddressZero);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
