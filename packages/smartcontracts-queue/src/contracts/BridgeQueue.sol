528d9683791961e422b0790a21d89d33e097d1f7
JJ Adonis <work@josejai.me>
2023-05-15 16:17:36 +0800

    add pragma version

--- packages/smartcontracts-queue/src/contracts/BridgeQueue.sol
+++ packages/smartcontracts-queue/src/contracts/BridgeQueue.sol
@@ -1,3 +1,4 @@
+pragma solidity 0.8.17;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
--- packages/smartcontracts-queue/src/contracts/test/TestToken.sol
+++ packages/smartcontracts-queue/src/contracts/test/TestToken.sol
@@ -1,3 +1,4 @@
+pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
