c385b9c82ece0d9d87de08b1001054e882f2d9e5
G9000 <leo.caesar@live.com>
2023-05-16 15:14:32 +0800

    fix liquidity page

--- apps/web/src/components/OverviewList.tsx
+++ apps/web/src/components/OverviewList.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 import { useDeFiScanContext } from "@contexts/DeFiScanContext";
-import { NetworkI, networks } from "@contexts/NetworkContext";
+import { NetworkI, useNetworkContext } from "@contexts/NetworkContext";
 import Image from "next/image";
 import NumericFormat from "@components/commons/NumericFormat";
 import BigNumber from "bignumber.js";
@@ -188,7 +188,9 @@ function TokenDetails({
 }
 
 export default function OverviewList({ balances }) {
-  const [firstNetwork, secondNetwork] = networks;
+  const { supportedTokens } = useNetworkContext();
+
+  const [firstNetwork, secondNetwork] = supportedTokens;
   const { isMobile } = useResponsive();
 
   const getAmount = (symbol: string, network): BigNumber => {
