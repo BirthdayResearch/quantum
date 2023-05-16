18c84cb27a1b7721542ff344717e2c5f74d40026
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2023-05-16 12:18:51 +0800

    feat(Contracts): Queue smart contracts (#880)
    * initial drafts for smart contract
    
    * small changes and add tests
    
    * add comments and tests
    
    * fix hardhat-etherscan dependency in packages/smartcontracts
    
    * fix dependencies in packages/smartcontractsv2
    
    * Renamed smartcontractsv2 directory
    
    * Rename files and smartcontracts
    
    * edit config files
    
    * edit dependencies, add deployment scripts
    
    * change error names
    
    * change smartcontractsQueue directory
    
    * change lock file
    
    * remove hardhat-etherscan dependency in package/smartcontracts
    
    * remove import to test ci/cd
    
    * change readme file and tsconfig.json
    
    * Change configs for package.json
    
    * remove reentrancy from smartcontract and delete hardcoded param in deployment scripts
    
    * updated package lock
    
    * Update packages/smartcontracts-queue/src/tests/BridgeOrderBook.bridgeToDeFiChain.spec.ts
    
    Co-authored-by: Abhishek kochar <43106142+Abhishekkochar@users.noreply.github.com>
    
    * add lint and format configuration
    
    * try version 0.8.16
    
    * change to 0.8.17 in smartcontracts-queue
    
    * add pragma version
    
    * remove postinstall
    
    * update to 0.8.18
    
    * add deployment scripts
    
    * add libvips-dev
    
    * remove libvips-dev
    
    * remove libvips-dev
    
    * minor name change
    
    * add back postinstall
    
    * remove postinstall
    
    ---------
    
    Co-authored-by: JJ Adonis <work@josejai.me>
    Co-authored-by: Abhishek kochar <43106142+Abhishekkochar@users.noreply.github.com>

---
+++ packages/smartcontracts-queue/src/scripts/utils/verify.ts
@@ -0,0 +1,26 @@
+import { run } from 'hardhat';
+
+export async function verify({ contractAddress, args, contract }: ContractInput) {
+  console.log('Verifying contract...');
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+      contract,
+    });
+  } catch (e) {
+    if ((e as Error).message.toLowerCase().includes('already verified')) {
+      console.log('ERROR');
+      console.log('Already verified!');
+    } else {
+      console.log('ERROR');
+      console.log(e);
+    }
+  }
+}
+
+interface ContractInput {
+  contractAddress: string;
+  args?: any;
+  contract?: string;
+}
