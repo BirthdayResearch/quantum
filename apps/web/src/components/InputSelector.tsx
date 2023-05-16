c313a65c0f2f3ccf241cef46b452d28024228402
veralygit <57183851+veralygit@users.noreply.github.com>
2023-05-16 14:27:58 +0800

    update tracking size for fonts

--- apps/web/src/components/InputSelector.tsx
+++ apps/web/src/components/InputSelector.tsx
@@ -184,7 +184,7 @@ export default function InputSelector({
   return (
     <div>
       {label && (
-        <span className="text-dark-900 pl-3 lg:pl-5 text-xs font-semibold lg:text-sm xl:tracking-wider">
+        <span className="text-dark-900 pl-3 lg:pl-5 text-xs font-semibold lg:text-sm xl:tracking-wider tracking-normal lg:tracking-[0.02em] ">
           {label}
         </span>
       )}
