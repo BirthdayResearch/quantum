3331a39a62bc0dd8c1a4f245c667f97c11160f2a
veralygit <57183851+veralygit@users.noreply.github.com>
2023-05-23 11:13:00 +0800

    fix: linting

--- apps/web/src/utils/sleep.ts
+++ apps/web/src/utils/sleep.ts
@@ -1,5 +1,6 @@
-export default function sleep(ms: number) {
-  return new Promise(function (resolve) {
+const sleep = (ms) =>
+  new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-}
+
+export default sleep;
