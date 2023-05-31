4fa8712e4ab4da8aa61f26e557ecd1b43d9a8108
nattadex <elocinnat99@gmail.com>
2023-05-31 11:52:43 +0800

    changed BTC to WBTC

--- apps/server/src/AppConfig.ts
+++ apps/server/src/AppConfig.ts
@@ -44,7 +44,7 @@ export function appConfig() {
       ethWalletPrivKey: process.env.ETHEREUM_WALLET_PRIVATE_KEY,
       queueTokensMinAmt: {
         ETH: process.env.ETH_MIN_QUEUE_AMT,
-        BTC: process.env.BTC_MIN_QUEUE_AMT,
+        BTC: process.env.WBTC_MIN_QUEUE_AMT,
         USDT: process.env.USDT_MIN_QUEUE_AMT,
         USDC: process.env.USDC_MIN_QUEUE_AMT,
         EUROC: process.env.EUROC_MIN_QUEUE_AMT,
