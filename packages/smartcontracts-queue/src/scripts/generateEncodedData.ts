1c39bea606b2f4034880da5efa32a81b197bd76f
Cuong  Truong <cuong@cakedefi.com>
2023-11-27 18:01:01 +0800

    add more to the script

--- packages/smartcontracts-queue/src/scripts/generateEncodedData.ts
+++ packages/smartcontracts-queue/src/scripts/generateEncodedData.ts
@@ -1,2 +1,3 @@
+// import { BridgeQueue__factory } from "../generated";
 
 // console.log(BridgeQueue__factory.createInterface().encodeFunctionData("addSupportedToken", [/*Ethereum address*/]));
--- packages/smartcontracts/src/scripts/generateEncodedData.ts
+++ packages/smartcontracts/src/scripts/generateEncodedData.ts
@@ -1,2 +1,3 @@
+// import { BridgeV1__factory } from "../generated";
 
 // console.log(BridgeV1__factory.createInterface().encodeFunctionData("addSupportedTokens", [/* Ethereum address */, /* A number */]))
